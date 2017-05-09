58af688dafbbdf202cc9280784b9c00828d49eb5
Ben Pardo <benpardo@Bens-MBP-4.lan>
2017-05-09 11:49:19 -0600

    adding text node insertion

--- reactive-vanilla.js
+++ reactive-vanilla.js
@@ -2,11 +2,19 @@
     browser, es6
 */
 
-function makeElement(type) {
+function makeElement(type, text) {
     "use strict";
-    return document.createElement(type);
+    const el = document.createElement(type);
+    const textNode = document.createTextNode(text);
+
+    el.appendChild(textNode);
+
+    return el;
 }
 
-const h1 = () => makeElement(`h1`);
+//const h1 = () => makeElement(`h1`);
 
-document.body.appendChild(h1());
+//document.body.appendChild(h1());
+const h1 = (text) => makeElement(`h1`, text);
+
+document.body.appendChild(h1(`Hello, world.`));

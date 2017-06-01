aff41140492fcdcf76906413af67d3a88e5e71a2
Mike Ricos via ux305 <mike.ricos@gmail.com>
2017-06-01 10:11:44 -0700

    Added jsdom code, not quiting yet

--- investigations/react/reactive-vanilla.js
+++ investigations/react/reactive-vanilla.js
@@ -42,8 +42,8 @@ function setStyles(el, styles) {
 
 
 function makeElement(type, textOrPropsOrChild, ...otherChildren) {
-    "use strict";
-    const el = document.createElement(type);
+   //"use strict";
+    const el = window.document.createElement(type);
 	
     if (Array.isArray(textOrPropsOrChild)) {
       appendArray(el, textOrPropsOrChild);
@@ -81,9 +81,64 @@ const header = (...args) => makeElement(`header`, ...args);
 const p = (...args) => makeElement(`p`, ...args);
 const span = (...args) => makeElement(`span`, ...args);
 
-document.body.appendChild(h1(
-    {
-        className: "title"
-    },
-    `Hello, world.`
-));
+/* Simple brute force way..
+
+document.body.appendChild(
+  div({ id: `app` },
+    header({ className: `header` },
+      h1({ className: `header__title` }, `Know It All`),
+      a(
+        {
+          className: `header__help`,
+          target: `_blank`,
+          rel: `noopener noreferrer`,
+          title: `Find out more about know it all`,
+          href: `https://hackernoon.com/what-you-dont-know-about-web-development-d7d631f5d468#.ex2yp6d64`,
+        },
+        `What is this?`
+      )
+    ),
+    div({ className: `skill-table` })
+  )
+);
+*/
+
+/* 
+  Compentents  are just fuctions named with Capital letters
+*/
+const Header = props => (
+  header({ className: `header` },
+    h1({ className: `header__title` }, `Know It All`),
+    a(
+      {
+        className: `header__help`,
+        target: `_blank`,
+        rel: `noopener noreferrer`,
+        title: `Find out more about know it all, version ${props.version}`,
+        href: `https://hackernoon.com/what-you-dont-know-about-web-development-d7d631f5d468#.ex2yp6d64`,
+      },
+      `What is this?`
+    )
+  )
+);
+const Table = props => div({ className: `skill-table` }, props.rows);
+
+const App = props => (
+  div({ id: `app` },
+    Header({ version: props.version }),
+    Table({ rows: props.rows })
+  )
+);
+
+// exports is in common.js modules
+if (typeof document === 'undefined'){
+  exports.App=App;
+}
+
+// If called from client..
+if (typeof document !== 'undefined'){
+  document.body.appendChild(App({
+    version: 002,
+    rows:3
+  }));
+}

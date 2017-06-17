db2a350be8e13798bfdad8dcd79221b9ba2010c2
Ben Pardo <benpardo@Bens-MBP-4.lan>
2017-06-17 17:10:16 -0600

    make tag beautifies html for nested tags

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-exports.makeTag = function (tagName,text){
-    return "<h1>InstaMarch: The Imaginary Jobs Program</h1>"
-}
+/*jslint node es6*/
+
+exports.makeTag = function (tagName, child) {
+    "use strict";
+    const tagCharacters = child.split("").reduce(function (total, character) {
+        if (character === "<" || character === ">") {
+            return total + 1;
+        }
+        return total;
+    }, 0);
+    if (tagCharacters >= 4) {
+        return "<" + tagName + ">" + "\n" + "    " + child + "\n" + "</" + tagName + ">";
+    }
+    return "<" + tagName + ">" + child + "</" + tagName + ">";
+
+};
--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,27 @@
-var test = require('tape');
-var app = require('../app.js');
- 
-test('make-h1', function (t) {
+/*jslint node, es6*/
+
+const test = require("tape");
+const app = require("../app.js");
+
+test("make-h1", function (t) {
+    "use strict";
     t.plan(1);
     t.equal(
         app.makeTag("h1", "InstaMarch: The Imaginary Jobs Program"),
         "<h1>InstaMarch: The Imaginary Jobs Program</h1>"
     );
-}
-);
+});
 
-test('make-header', function (t) {
+test("make-header", function (t) {
+    "use strict";
     t.plan(1);
     t.equal(
         app.makeTag("header",
-            "<h1>InstaMarch: The Imaginary Jobs Program</h1>"
-        ),
+                "<h1>InstaMarch: The Imaginary Jobs Program</h1>"),
         (
             "<header>\n"
-            +"    <h1>InstaMarch: The Imaginary Jobs Program</h1>\n"
-            +"</header>"
+            + "    <h1>InstaMarch: The Imaginary Jobs Program</h1>\n"
+            + "</header>"
         )
-   )       
-}
-);
+    );
+});

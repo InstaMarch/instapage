a2609a175a64e5261d9c70b19777940e3e275d57
Ben Pardo <benpardo@Bens-MBP-4.lan>
2017-06-17 15:26:33 -0600

    adding basic h1 test

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,7 @@
-/*
-    Import has a weird way of making a default object as the interface.
-    It also enables bad code by allowing a user to change the imported file's name. JSLint forbids this option.
-    babel test.js --presets es2015
-    https://babeljs.io/docs/usage/cli/
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
-    http://www.jslint.com/help.html
-*/
-
-import fs from 'fs';
-
-let funk = (input) => input * 2;
-
-console.log(funk(2));
-
 var test = require('tape');
  
 test('timing test', function (t) {
-    t.plan(2);
+    t.plan(1);
     
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
-    
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
+    t.equal(makeTag("h1", "InstaMarch: The Imaginary Jobs Program"),"<h1>InstaMarch: The Imaginary Jobs Program</h1>");
 });

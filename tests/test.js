573d1fb63d32229077b8d9f3eba2bdcf89ede052
Mike Ricos via ux305 <mike.ricos@gmail.com>
2017-06-17 15:19:35 -0700

    added make-header test

--- tests/test.js
+++ tests/test.js
@@ -7,4 +7,20 @@ test('make-h1', function (t) {
         app.makeTag("h1", "InstaMarch: The Imaginary Jobs Program"),
         "<h1>InstaMarch: The Imaginary Jobs Program</h1>"
     );
-});
+}
+);
+
+test('make-header', function (t) {
+    t.plan(1);
+    t.equal(
+        app.makeTag("header",
+            "<h1>InstaMarch: The Imaginary Jobs Program</h1>"
+        ),
+        (
+            "<header>\n"
+            +"    <h1>InstaMarch: The Imaginary Jobs Program</h1>\n"
+            +"</header>"
+        )
+   )       
+}
+);

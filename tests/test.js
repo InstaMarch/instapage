aa62ccf4b1bf0404cd18046d6037e66e3ddf8944
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-06-27 14:34:34 -0600

    adding a test for nesting an element as a child while the parent is two elements

--- tests/test.js
+++ tests/test.js
@@ -12,7 +12,7 @@ test("make h1", function (t) {
     );
 });
 
-test("make header", function (t) {
+test("make header component", function (t) {
     "use strict";
     t.plan(1);
     t.equal(
@@ -46,5 +46,22 @@ test("Make an h1 with two attributes", function (t) {
             "id": "tag-id"
         }, "InstaMarch: The Imaginary Jobs Program"),
         "<h1 class='page-title' id='tag-id'>InstaMarch: The Imaginary Jobs Program</h1>"
+    );
+});
+
+test("make header component with attributes", function (t) {
+    "use strict";
+    t.plan(1);
+    t.equal(
+        app.makeTag("header", {
+            "class": "header",
+            "id": "header"
+        },
+        "<h1>InstaMarch: The Imaginary Jobs Program</h1>"),
+        (
+            "<header class='header' id='id'>\n"
+            + "    <h1>InstaMarch: The Imaginary Jobs Program</h1>\n"
+            + "</header>"
+        )
     );
 });

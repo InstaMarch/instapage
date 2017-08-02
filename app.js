0f95444b0eb09ba53b4b5151eb001b32e6ac3756
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-08-01 18:38:48 -0600

    adding support for multiple child element creation

--- app.js
+++ app.js
@@ -4,35 +4,39 @@ const {makeAttributesHtml} = require("./helpers");
 exports.makeTag = function (tagName, attributes, child) {
     "use strict";
 
+    if (typeof(child) === "string") {
 
+        const tagCharacters = child.split("").reduce(function (total, character) {
+            if (character === "<" || character === ">") {
+                return total + 1;
+            }
+            return total;
+        }, 0);
 
-    const tagCharacters = child.split("").reduce(function (total, character) {
-        if (character === "<" || character === ">") {
-            return total + 1;
+        const numberOfAttributes = Object.keys(attributes).length;
+
+        if (tagCharacters >= 4 && numberOfAttributes > 0) {  // if we're two tags deep..
+            return "<" + tagName
+                    + " " + makeAttributesHtml(attributes) + ">" + "\n" + "    "
+                    + child + "\n" + "</" + tagName + ">";
         }
-        return total;
-    }, 0);
 
-    const numberOfAttributes = Object.keys(attributes).length;
+        if (tagCharacters >= 4 && numberOfAttributes === 0) {  // if we're two tags deep..
+            return "<" + tagName + ">" + "\n" + "    "
+                    + child + "\n" + "</" + tagName + ">";
+        }
 
-    if (tagCharacters >= 4 && numberOfAttributes > 0) {  // if we're two tags deep..
-        return "<" + tagName
-                + " " + makeAttributesHtml(attributes) + ">" + "\n" + "    "
-                + child + "\n" + "</" + tagName + ">";
+        if (numberOfAttributes > 0) {
+            return "<" + tagName + " " + makeAttributesHtml(attributes) + ">"
+                    + child + "</" + tagName + ">";
+        }
+
+        return "<" + tagName + ">" + child + "</" + tagName + ">";
+
     }
+    if (Array.isArray(child)) {
+        const childrenHtml = child.reduce((totalHtml, element) => totalHtml + "    " + element + "\n", "");
 
-    if (tagCharacters >= 4 && numberOfAttributes === 0) {  // if we're two tags deep..
-        return "<" + tagName + ">" + "\n" + "    "
-                + child + "\n" + "</" + tagName + ">";
+        return "<" + tagName + ">" + "\n" + childrenHtml + "</" + tagName + ">";
     }
-
-    if (numberOfAttributes > 0) {
-        return "<" + tagName + " " + makeAttributesHtml(attributes) + ">"
-                + child + "</" + tagName + ">";
-    }
-
-    return "<" + tagName + ">" + child + "</" + tagName + ">";
-
-
-
 };

4b2e0905a67637f8b2a8a0c4d7987e2f58b865f4
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-08-23 00:16:48 -0600

    adding build file for InstaMarch homepage

---
+++ build.js
@@ -0,0 +1,24 @@
+const {makeTag} = require("./app");
+const fs = require("fs");
+
+const html = makeTag("html", {},
+    [
+        makeTag("head", {}, 
+            [
+                makeTag("link", {
+                    rel: "stylesheet",
+                    href: "index.css"
+                }, "SET"
+                )
+            ]),
+        makeTag("body", {},
+            makeTag("p", {}, "hello World")
+        )
+]);
+
+fs.writeFile("index.html", html, function (error) {
+    if(error) {
+        throw error;
+    }
+    console.log("website built");
+});
